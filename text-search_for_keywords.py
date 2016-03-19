d4f4823073806b86ff9c57e97479b47a97a81553
perkel666 <ciotazemnie@gmail.com>
2016-03-19 14:10:15 +0100

    - working on my text event handling

--- text-search_for_keywords.py
+++ text-search_for_keywords.py
@@ -1,1 +1,34 @@
 __author__ = 'Perkel'
+
+
+class ReadTextFile():
+    def __init__(self, text_file):
+        self.whole_text = text_file.read().split('\n')
+
+    def print_text(self):
+        print ''
+        print "====WHOLE FILE======"
+        for line in self.whole_text:
+            print line
+        print "===================="
+        print ''
+
+    def print_events_list(self):
+        print ''
+        print "========EVENTS=========="
+        count = 0
+        for line in self.whole_text:
+            count += 1
+            if "EVENT" in line:
+                print line, "line :", count
+        print "========================"
+        print ''
+
+
+
+crs = open("data/text/key_word_test.txt", "r")
+
+categorized_text_file = ReadTextFile(crs)
+
+categorized_text_file.print_text()
+categorized_text_file.print_events_list()
