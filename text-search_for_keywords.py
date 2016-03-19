10b1e84a438b1a4df6cddf4350ace0d92e4fcd4b
perkel666 <ciotazemnie@gmail.com>
2016-03-19 19:47:30 +0100

    - added text parsing method

--- text-search_for_keywords.py
+++ text-search_for_keywords.py
@@ -3,9 +3,27 @@ __author__ = 'Perkel'
 import re
 
 
+class TextParsing():
+
+    @ staticmethod
+    def parse_text(text):
+        text_copy = text
+        new_text = []
+        for line in text_copy:
+            if line != '':
+                parsed_line = line.lstrip()
+                new_text.append(parsed_line)
+
+        return new_text
+
+
 class ReadTextFile():
     def __init__(self, text_file):
-        self.whole_text = text_file.read().split('\n')
+        self.whole_text = TextParsing.parse_text(text_file.read().split('\n'))
+
+    def parse_text_file(self):
+        text = TextParsing.parse_text(self.whole_text)
+        return text
 
     def print_text(self):
         print ''
@@ -38,21 +56,24 @@ class ReadTextFile():
     def print_event(self, event_name):
         events = self.print_events_list()
 
-        count = -1
+        """ Look for event in event list
+            Once found decide start of event line and end of event line
+        """
 
         print_start = None
         print_end = None
 
         next_event = None
 
+        count = -1  # -1 because i need to get over list starting with 0 twice so -1 and not 0
         for event in events:
             count += 1
             if event_name in event[0]:
-                print_start = event[1]+3
+                print_start = event[1]+2
                 next_event = count + 1
 
         print_end = events[next_event]
-        print_end = print_end[1]
+        print_end = print_end[1]-1
         print "==========="
         print print_start
         print print_end
@@ -63,24 +84,28 @@ class ReadTextFile():
         print "=========EVENT========="
         full_event = []
         for x in self.whole_text:
-            count += 1
-            if count >= print_start and count <= print_end:
+            count2 += 1
+            if count2 >= print_start and count2 <= print_end:
                 if x != '':
                     full_event.append(x)
 
+        event_content = []
         for line in full_event:
+            x = line.lstrip()
+            event_content.append(x)
             print line.lstrip()
 
         print "=========EVENT========="
 
+        print event_content
+
+        return event_content
 
 
 
 
-        if count == 1:
-            print "There is such a file"
-        else:
-            print "No such event"
+
+
 
 
 
@@ -94,3 +119,4 @@ categorized_text_file = ReadTextFile(crs)
 
 categorized_text_file.print_events_list()
 categorized_text_file.print_event("st002")
+categorized_text_file.print_text()
