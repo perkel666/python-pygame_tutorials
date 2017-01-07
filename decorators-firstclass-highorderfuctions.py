04a3a57e4c2033bbb35dd4d3d6e7ab049f79321d
Perkel <ciotazemnie@gmail.com>
2017-01-07 14:06:08 +0100

    - added decorators

--- decorators-firstclass-highorderfuctions.py
+++ decorators-firstclass-highorderfuctions.py
@@ -24,3 +24,28 @@ ITALIC = outer_function('ITALIC')
 
 BOLD("Let's get down to business")
 ITALIC("Let's get down to business")
+
+
+# DECORATORS
+
+
+def decorator_function(original_function):
+    """
+    This allows to add additional code inside premade function
+    :param original_function: original function you want to change.
+    :return: returns original function with new code in it.
+    """
+    def wrapper_function():
+        # insert code here
+        print 'TO BAD'
+
+        # insert code here
+        original_function()
+    return wrapper_function
+
+@decorator_function
+def print_number():
+    print '12'
+
+print_number()
+
